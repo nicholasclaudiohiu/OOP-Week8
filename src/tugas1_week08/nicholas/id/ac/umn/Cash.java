2adcc141362887c9ef7fde3b787bd48116e840c4
Nicholas Claudio <nicholasclaudiohiu@gmail.com>
2025-10-29 18:52:34 +0700

    cash class

---
+++ src/tugas1_week08/nicholas/id/ac/umn/Cash.java
@@ -0,0 +1,19 @@
+package tugas1_week08.nicholas.id.ac.umn;
+
+public class Cash extends Payment{
+	public Cash(Item item) {
+		super(item);
+	}
+	
+	public int pay() {
+		if(isPaidOff) {
+			return 0;
+		}
+		isPaidOff = true;
+		return this.item.getPrice();
+	}
+	
+	public String getClassName() {
+		return "CASH";
+	}
+}
