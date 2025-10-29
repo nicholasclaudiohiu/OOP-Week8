6db306777a48ccae824d72db244cf792914f630b
Nicholas Claudio <nicholasclaudiohiu@gmail.com>
2025-10-29 18:51:24 +0700

    class Payment

---
+++ src/tugas1_week08/nicholas/id/ac/umn/Payment.java
@@ -0,0 +1,45 @@
+package tugas1_week08.nicholas.id.ac.umn;
+
+public abstract class Payment {
+	protected boolean isPaidOff;
+	protected Item item;
+	
+	public abstract int pay();
+	
+	public Payment() {
+		this.isPaidOff = false;
+		this.item = null;
+	}
+	
+	public Payment(Item item) {
+		this.isPaidOff = false;
+		this.item = item;
+	}
+
+	public boolean isPaidOff() {
+		return isPaidOff;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public String getItemName() {
+		return item.getName();
+	}
+	
+	public String getStatus() {
+		if(isPaidOff) {
+			return "FINISHED";
+		}
+		return "ON PROGRESS";
+	}
+	
+	public int getRemainingAmount() {
+		if(isPaidOff) {
+			return 0;
+		}
+		return item.getPrice();
+	}
+	
+}
